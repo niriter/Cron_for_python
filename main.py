e05b04437fa686236bf7dcf13594e7efc90f2448
Nikita Kasyanenko <pro100.vce@gmail.com>
2019-11-13 23:11:17 +0100

    Deleted file, and fix every few minutes tasks

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ from threading import Thread
 # ------------- минута(0—59)
 
 class Cron_Stable():
+    def __init__(self, quick_run=False):
+        if quick_run == True:
+            self.quick_run = True
+            self.load_tasks()
+            self.run()
+        else:
+            self.quick_run = False
+
     def run(self):
         process = Thread(target=self.start)
         process.start()
@@ -32,31 +40,43 @@ class Cron_Stable():
             time.sleep(seconds_left)
 
     def load_tasks(self):
-        with open("cron.txt", "r") as myfile:
-            self.tasks = myfile.readlines()
+        if self.quick_run:
+            with open("libraries/cron_stable/cron.txt", "r") as myfile:
+                self.tasks = myfile.readlines()
+        else:
+            with open("cron.txt", "r") as myfile:
+                self.tasks = myfile.readlines()
         for indx, task in enumerate(self.tasks):
             self.tasks[indx] = task.replace('\n', '')
+
 
     def needs_start(self):
         for task in self.tasks:
             if self.is_task_callable(task):
                 task_file = task.split()
                 task_file = str(task_file[-1])
+                if self.quick_run:
+                    task_file = task_file.replace('../../', '')
                 os.system("python "+task_file)
         return
 
     def is_task_callable(self, task):
         tmp = task.split()
 
-        if tmp[0] == '*' or ( ('/' in tmp[0]) and (int(self.time_now.strftime("%M")) % int(tmp[0].split('/')[-1]) == 0) ) or int(self.time_now.strftime("%M")) == int(tmp[0]): #minute
-            if tmp[1] == '*' or (('/' in tmp[1]) and (int(self.time_now.strftime("%H")) % int(tmp[1].split('/')[-1]) == 0)) or int(self.time_now.strftime("%H")) == int(tmp[1]): #hour
-                if tmp[2] == '*' or (('/' in tmp[2]) and (int(self.time_now.strftime("%d")) % int(tmp[2].split('/')[-1]) == 0)) or int(self.time_now.strftime("%d")) == int(tmp[2]): #day
-                    if tmp[3] == '*' or (('/' in tmp[3]) and (int(self.time_now.strftime("%m")) % int(tmp[3].split('/')[-1]) == 0)) or int(self.time_now.strftime("%m")) == int(tmp[3]): #hour
+        if tmp[0] == '*' or ( ('/' in tmp[0]) and (int(self.time_now.strftime("%M")) % int(tmp[0].split('/')[-1]) == 0)) or (not ('/' in tmp[0]) and int(self.time_now.strftime("%M")) == int(tmp[0])): #minute
+            if tmp[1] == '*' or (('/' in tmp[1]) and (int(self.time_now.strftime("%H")) % int(tmp[1].split('/')[-1]) == 0)) or (not ('/' in tmp[0]) and int(self.time_now.strftime("%H")) == int(tmp[0])): #hour
+                if tmp[2] == '*' or (('/' in tmp[2]) and (int(self.time_now.strftime("%d")) % int(tmp[2].split('/')[-1]) == 0)) or (not ('/' in tmp[0]) and int(self.time_now.strftime("%d")) == int(tmp[0])): #day
+                    if tmp[3] == '*' or (('/' in tmp[3]) and (int(self.time_now.strftime("%m")) % int(tmp[3].split('/')[-1]) == 0)) or (not ('/' in tmp[0]) and int(self.time_now.strftime("%m")) == int(tmp[0])): #month
                         return True
                     else: return False
                 else: return False
             else: return False
         else: return False
+
+    def delete_modules_tasks(self):
+        if os.path.exists('modules_cron.txt'):
+            return
+
 
 if __name__ == "__main__":
     cron = Cron_Stable()
